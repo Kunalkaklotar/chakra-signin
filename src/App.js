fbb7960517a2e700785dc0f2a8bb9a6c48859a3a
Kunalkaklotar <kunalkaklotar@gmail.com>
2023-04-04 10:57:12 +0530

    chakra

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import { Button, Center, Container, Heading, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
+import { Button, Center, Container, Heading, Input, InputGroup, InputRightElement, Box, Flex, Image } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
   const [show, setShow] = React.useState(false)
   const handleClick = () => setShow(!show)
   return (
+    <Box>
+      <Box>
+        <Flex>
+          <Center><Image src='https://raw.githubusercontent.com/chakra-ui/chakra-ui/main/media/logo-colored@2x.png?raw=true' w='120px' ml='50px' /></Center>
+          <Center pl='750px'>
+            <Button m='0 10px' variant='ghost' colorScheme='white'>   Home  </Button>
+            <Button m='0 10px' variant='ghost' colorScheme='white'>  About  </Button>
+            <Button m='0 10px' variant='ghost' colorScheme='white'>   Help  </Button>
+            <Button m='0 10px' variant='ghost' colorScheme='white'> Account </Button>
+          </Center>
+        </Flex>
+      </Box>
+      <Box>
         <Container border='1px solid black' color='black' mt='100px' p='50px 25px'>
           <Center>
             <Heading>LOGIN PAGE</Heading>
           </Center>
-          <Input display='inline-block' mt='40px' w='100%' _placeholder={{ color: 'black' }} placeholder='Name' />
-          <Input display='inline-block' mt='20px' w='100%' _placeholder={{ color: 'black' }} placeholder='Email' />
+          <Input mt='40px' w='100%' _placeholder={{ color: 'black' }} placeholder='Name' />
+          <Input mt='20px' w='100%' _placeholder={{ color: 'black' }} placeholder='Email' />
           <InputGroup mt='20px' size='md'>
-          <Input border='1px solid black' pr='4.5rem' type={show ? 'text' : 'password'} placeholder='Password' _placeholder={{ color: 'black' }}/>
+            <Input border='1px solid black' pr='4.5rem' type={show ? 'text' : 'password'} placeholder='Password' _placeholder={{ color: 'black' }} />
             <InputRightElement width='100px'>
-              <Button h='25px' color='black' bg='transparent' 
-                
+              <Button h='25px' color='black' bg='transparent'
+
                 onClick={handleClick}>
-                  {show ? 'Hide' : 'Show'}
+                {show ? 'Hide' : 'Show'}
 
               </Button>
             </InputRightElement>
@@ -29,6 +42,8 @@ function App() {
             </Button>
           </Center>
         </Container>
+      </Box>
+    </Box>
   );
 }
 
